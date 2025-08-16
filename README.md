906d17270c8940e158b848ea5b266f7df56a2744
Nikolay Roll <rol.nikolay@gmail.com>
2025-08-16 14:27:25 +0300

    readme and small fixes

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # bookbot
+
 bookbot is a project built as part of the boot.dev course
 
+# How to use
+
+It is a CLI command that will count the number of characters and give you a split of characters.
+Example:
+![example how to use](public/readme/example_of_use.png)
+
+To get things going:
+
+1. Install python3
+2. clone this repo
+3. Run the CLI command:
+
+```
+python3 main.py {path_to_the_book}
+```
+
+![run command python3 main.py books/prideandprejudice.txt](public/readme/image.png)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from stats import count_words, count_characters, dict_to_sorted_list
 import sys
 
@@ -9,10 +10,14 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python main.py <file_path>")
         sys.exit(1)
-        
-    with get_book_text(sys.argv[1]) as f:
+
+    file_path = sys.argv[1]
+    if not os.path.exists(file_path):
+        print("File not found:", file_path)
+        return
+
+    with get_book_text(file_path) as f:
         text=f.read()
-        print(text)
         count_words(text)
         dict_to_sorted_list(count_characters(text))
 
